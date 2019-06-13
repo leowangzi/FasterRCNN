405d668589a338db4ab2145e06d4ccdc8ff89fba
leowangzi <leowangzi@163.com>
2019-06-13 15:13:58 +0800

    remove script files

--- script/test.sh
+++ script/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-
+cd ..
 
 CUDA_VISIBLE_DEVICES=7  python test_net.py --dataset pascal_voc --net res101 --checksession 1 --checkepoch 16 --checkpoint 10021 --cuda
--- script/train.sh
+++ script/train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-
+cd ..
 
 CUDA_VISIBLE_DEVICES=2  python trainval_net.py --dataset pascal_voc --net res101 --bs 1 --nw 8 --lr 1e-3 --lr_decay_step 5 --epochs 20 --cuda
